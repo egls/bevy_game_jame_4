d2bf35a0066090378a6921e85df9ead70a0139d0
stefan <stefan.egli@gmail.com>
2023-12-03 11:31:35 +0100

    spawn_asteroids

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@
 // Feel free to delete this line.
 #![allow(clippy::too_many_arguments, clippy::type_complexity)]
 
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PrimaryWindow};
+use rand::prelude::*;
 
 fn main() {
     App::new()
@@ -18,16 +19,13 @@ fn main() {
         }))
         //.add_systems(Startup, startup_screen)
         .add_systems(Startup, setup)
+        .add_systems(Startup, spawn_player)
+        .add_systems(Startup, spawn_asteroid)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("planets/planet00.png"),
-        transform: Transform::from_scale(Vec3::splat(0.15)),
-        ..Default::default()
-    });
 }
 
 fn startup_screen(mut commands: Commands, asset_server: Res<AssetServer>) {
@@ -36,3 +34,42 @@ fn startup_screen(mut commands: Commands, asset_server: Res<AssetServer>) {
         ..Default::default()
     });
 }
+
+
+fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("planets/planet00.png"),
+        transform: Transform::from_scale(Vec3::splat(0.15)),
+        ..Default::default()
+    });
+}
+
+fn spawn_asteroid(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>, 
+    asset_server: Res<AssetServer>) {
+    
+
+    let window = window_query.get_single().unwrap();
+
+    for _ in 0..1000 {
+        let x = random::<f32>() * window.width() - window.width() / 2.0;  
+        let y = random::<f32>() * window.height() - window.height() / 2.0;
+
+        commands.spawn(
+            SpriteBundle {
+            texture: asset_server.load("asteroids/meteorGrey_tiny1.png"),
+            transform: Transform::from_xyz(x, y, 0.0),
+            ..default()
+        });
+
+    }
+    
+    
+    //commands.spawn(SpriteBundle {
+    //    texture: asset_server.load("asteroids/asteroid00.png"),
+    //    transform: Transform::from_scale(Vec3::splat(0.15)),
+    //    ..Default::default()
+    //    });
+}
+
